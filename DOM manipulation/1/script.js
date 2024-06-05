a68c85beedb3b8920337388c522b8820b68a7640
Dimm <dmzrfqi@gmail.com>
2024-06-05 23:00:54 +0700

    dom manip n dom selection

---
+++ DOM manipulation/1/script.js
@@ -0,0 +1,16 @@
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>Dimas</em>";
+
+// const sectionA = document.querySelector("#a")
+// window.addEventListener("scroll", function() {
+//     sectionA.classList.toggle("sticky", window.scrollY > 0) 
+// })
+
+// const judul = document.querySelector("#judul");
+// judul.style.color = "lightblue"
+
+// const judul = document.getElementsByTagName("h1")[0];
+
+// judul.setAttribute('name', 'palapas')
+// const p2 = document.querySelector(".p2");
+// p2.setAttribute("class", "p2.add")
---
+++ DOM manipulation/2/script.js
@@ -0,0 +1,47 @@
+// DOM Manipulation
+// create new element
+const newP = document.createElement("p");
+const newText = document.createTextNode("Paragraf baru");
+// simpan text ke dalam paragraph
+newP.appendChild(newText);
+// simpan newP di akhir section A
+
+const sectionA = document.getElementById("a");
+sectionA.appendChild(newP);
+
+
+
+const liNew = document.createElement("li");
+const newLi = document.createTextNode("List baru");
+
+liNew.appendChild(newLi);
+
+const ulList = document.querySelector('section#b ul');
+const li2 = ulList.querySelector("li:nth-child(2)");
+ulList.insertBefore(liNew, li2);
+
+
+const link = document.getElementsByTagName('a')[0];
+sectionA.removeChild(link);
+
+// const sectionB = document.getElementById('b');
+// const title = sectionB.getElementsByTagName("p")[0];
+
+// const titleWrap = document.createElement('h2');
+// const newTitle = document.createTextNode("Hello world!");
+// titleWrap.appendChild(newTitle);
+
+// sectionB.replaceChild(titleWrap, title);
+const sectionB = document.getElementById('b');
+const title = sectionB.getElementsByTagName("p")[0];
+
+const titleWrap = document.createElement('h2');
+const newTitle = document.createTextNode("Hello world!");
+
+titleWrap.append(newTitle);
+title.replaceWith(titleWrap);
+
+
+titleWrap.style.backgroundColor = "lightblue";
+newP.style.backgroundColor = "salmon";
+liNew.style.backgroundColor = "orange";
